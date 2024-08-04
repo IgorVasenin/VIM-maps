df9ba5919a67db8efcd3fe557a83ca4144d5f038
IgorVasenin <im.vasenin@gmail.com>
2024-08-05 02:00:07 +0500

    Update and rename maps.php to maps.html

--- maps.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo: 'VIM maps'
-<iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/ru/map/map_1059965?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true"></iframe><p><a href="//umap.openstreetmap.fr/ru/map/map_1059965?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true">Смотреть в полноэкранном режиме</a></p>
-?>
